84d7f75d5861cf65f9427867f10283a203cab6c3
EarlyBloomer-byte <adebayoj383@gmail.com>
2025-12-26 17:48:13 +0100

    Returned the pie layout value to original

--- visuals.py
+++ visuals.py
@@ -38,10 +38,10 @@ def plot_donut_chart(df):
     labels = ['Recoveries', 'Deaths']
     values = [total_recoveries, total_deaths]
     
-    fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.40)]) # .6 , 300
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.6)]) # .6 , 300
     fig.update_layout(
         title="Outcome Distribution", 
-        height=290,
+        height=300,
         margin=dict(l=20, r=20, t=40, b=20)
     )
     return fig
